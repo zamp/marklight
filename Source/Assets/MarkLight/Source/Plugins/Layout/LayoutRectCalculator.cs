4741a7ec7e10018f52c89f228e35887838578480
JCThePants <jcthepants@gmail.com>
2017-03-10 12:19:51 -0800

    Allow view to be moved by margin if there is room.
    Previously the view is the specified size minus margins. This can cause the view to be smaller than desired or even collapse completely if the a margin is larger than the size of the view.
    
    LayoutRectCalculator changed to allow margin to displace view if there is room. If the size of the view is specified in pixels, the view size will be maintained against the margin. The margin side that matches the alignment will be maintained while the opposite margin will not be honored. For percent based margins, the size of the view may be reduced in order to honor the margins.

--- Source/Assets/MarkLight/Source/Plugins/Layout/LayoutRectCalculator.cs
+++ Source/Assets/MarkLight/Source/Plugins/Layout/LayoutRectCalculator.cs
@@ -14,8 +14,6 @@ namespace MarkLight
         /// </summary>
         public static readonly LayoutRectCalculator Default = new LayoutRectCalculator();
 
-        private static readonly ElementMargin EmptyMargin = new ElementMargin();
-
         #endregion
 
         #region Methods
@@ -35,51 +33,26 @@ namespace MarkLight
                 ? data.View.OverrideHeight.Value
                 : data.AspectHeight;
 
-            var margin = data.PositionType == ElementPositionType.Normal
-                ? data.Margin
-                : EmptyMargin;
-
             // horizontal alignment
             var minMaxX = GetMinMaxX(data, width);
 
             //  vertical alignment
             var minMaxY = GetMinMaxY(data, height);
 
-            data.AnchorMin = new Vector2(minMaxX.Min + margin.Left.Percent, minMaxY.Min + margin.Bottom.Percent);
-            data.AnchorMax = new Vector2(minMaxX.Max - margin.Right.Percent, minMaxY.Max - margin.Top.Percent);
+            data.AnchorMin = new Vector2(minMaxX.Min, minMaxY.Min);
+            data.AnchorMax = new Vector2(minMaxX.Max, minMaxY.Max);
+
+            var pixelWidth = data.AspectPixelWidth;
+            var pixelHeight = data.AspectPixelHeight;
 
             // positioning and margins
             data.OffsetMin = new Vector2(
-
-                minMaxX.OffsetMin
-                    + data.MarginLeftPixels
-                    + data.OffsetLeftPixels
-                    - data.OffsetRightPixels
-                    + data.OffsetFromParentLeftPixels
-                    - data.OffsetFromParentRightPixels,
-
-                minMaxY.OffsetMin
-                    + data.MarginBottomPixels
-                    - data.OffsetTopPixels
-                    + data.OffsetBottomPixels
-                    - data.OffsetFromParentTopPixels
-                    + data.OffsetFromParentBottomPixels);
+                GetOffsetMinX(minMaxX.OffsetMin, pixelWidth, data),
+                GetOffsetMinY(minMaxY.OffsetMin, pixelHeight, data));
 
             data.OffsetMax = new Vector2(
-
-                minMaxX.OffsetMax
-                    - data.MarginRightPixels
-                    + data.OffsetLeftPixels
-                    - data.OffsetRightPixels
-                    + data.OffsetFromParentLeftPixels
-                    - data.OffsetFromParentRightPixels,
-
-                minMaxY.OffsetMax
-                    - data.MarginTopPixels
-                    - data.OffsetTopPixels
-                    + data.OffsetBottomPixels
-                    - data.OffsetFromParentTopPixels
-                    + data.OffsetFromParentBottomPixels);
+                GetOffsetMaxX(minMaxX.OffsetMax, pixelWidth, data),
+                GetOffsetMaxY(minMaxY.OffsetMax, pixelHeight, data));
 
             data.AnchoredPosition = new Vector2(
                 data.OffsetMin.x / 2.0f + data.OffsetMax.x / 2.0f,
@@ -89,7 +62,8 @@ namespace MarkLight
         /// <summary>
         /// Get horizontal rect min/max values.
         /// </summary>
-        protected virtual MinMax GetMinMaxX(LayoutData data, ElementSize width) {
+        protected virtual MinMax GetMinMaxX(LayoutData data, ElementSize width)
+        {
 
             var result = new MinMax();
 
@@ -145,6 +119,183 @@ namespace MarkLight
                 result.Max = 0.5f + height.Percent / 2f;
                 result.OffsetMin = -height.Pixels / 2f;
                 result.OffsetMax = height.Pixels / 2f;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Get rect OffsetMin X component value. (Left)
+        /// </summary>
+        protected virtual float GetOffsetMinX(float offsetMin, float pixelWidth, LayoutData data)
+        {
+            var result = offsetMin
+                   + data.MarginLeftPixels
+                   + data.OffsetLeftPixels
+                   - data.OffsetRightPixels
+                   + data.OffsetFromParentLeftPixels
+                   - data.OffsetFromParentRightPixels;
+
+            //if (Math.Abs(data.MarginRightPixels) < 0.00001f)
+            //    return result;
+
+            var isLeftAligned = data.Alignment.HasFlag(ElementAlignment.Left);
+            var isRightAligned = data.Alignment.HasFlag(ElementAlignment.Right);
+
+            // make sure left and right edges do not overlap if there is room to move entire view
+
+            var container = data.PaddedContainerPixelWidth - data.MarginLeftPixels;
+            var width = pixelWidth + data.MarginRightPixels;
+
+            // Positive delta means there is additional room to move the view in order to maintain width.
+            var delta = container - width;
+            if (delta > 0)
+            {
+                if (isLeftAligned)
+                {
+                    // Left aligned
+                    result -= Mathf.Min(data.MarginRightPixels, delta);
+                }
+                else if (!isRightAligned)
+                {
+                    // Center aligned
+                    result -= Mathf.Min(data.MarginRightPixels, delta) / 2f;
+                }
+            }
+            else if (delta < 0 && data.Height.Unit == ElementSizeUnit.Pixels)
+            {
+                // force height of pixel unit value
+                result -= Mathf.Min(data.MarginRightPixels, Mathf.Abs(delta));
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Get rect OffsetMin Y component value. (Bottom)
+        /// </summary>
+        protected virtual float GetOffsetMinY(float offsetMin, float pixelHeight, LayoutData data)
+        {
+            var result = offsetMin
+                   + data.MarginBottomPixels
+                   - data.OffsetTopPixels
+                   + data.OffsetBottomPixels
+                   - data.OffsetFromParentTopPixels
+                   + data.OffsetFromParentBottomPixels;
+
+            var isTopAligned = data.Alignment.HasFlag(ElementAlignment.Top);
+            var isBottomAligned = data.Alignment.HasFlag(ElementAlignment.Bottom);
+
+            // make sure top and bottom edges do not overlap if there is room to move entire view
+            var container = data.PaddedContainerPixelHeight - data.MarginBottomPixels;
+            var height = pixelHeight + data.MarginTopPixels;
+
+            // Positive delta means there is additional room to move the view in order to maintain height.
+            var delta = container - height;
+            if (delta > 0)
+            {
+                if (isTopAligned)
+                {
+                    // Top aligned
+                    result -= Mathf.Min(data.MarginTopPixels, delta);
+                }
+                else if (!isBottomAligned)
+                {
+                    // Center aligned
+                    result += Mathf.Min(data.MarginTopPixels, delta) / 2f;
+                }
+            }
+            else if (delta < 0 && data.Height.Unit == ElementSizeUnit.Pixels)
+            {
+                // force height of pixel unit value
+                result -= Mathf.Min(data.MarginTopPixels, Mathf.Abs(delta));
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Get rect OffsetMax X component value. (Right)
+        /// </summary>
+        protected virtual float GetOffsetMaxX(float offsetMax, float pixelWidth, LayoutData data)
+        {
+            var result = offsetMax
+                   - data.MarginRightPixels
+                   + data.OffsetLeftPixels
+                   - data.OffsetRightPixels
+                   + data.OffsetFromParentLeftPixels
+                   - data.OffsetFromParentRightPixels;
+
+            var isLeftAligned = data.Alignment.HasFlag(ElementAlignment.Left);
+            var isRightAligned = data.Alignment.HasFlag(ElementAlignment.Right);
+
+            // make sure left and right edges do not overlap if there is room to move entire view
+
+            var container = data.PaddedContainerPixelWidth - data.MarginRightPixels;
+            var width = pixelWidth + data.MarginLeftPixels;
+
+            // Positive delta means there is additional room to move the view in order to maintain width.
+            var delta = container - width;
+            if (delta > 0)
+            {
+                if (isLeftAligned)
+                {
+                    // Right aligned
+                    result += Mathf.Min(data.MarginLeftPixels, delta);
+                }
+                else if (!isRightAligned)
+                {
+                    // Center aligned
+                    result += Mathf.Min(data.MarginLeftPixels, delta) / 2f;
+                }
+            }
+            else if (delta < 0 && data.Height.Unit == ElementSizeUnit.Pixels)
+            {
+                // force height of pixel unit value
+                result += Mathf.Min(data.MarginLeftPixels, Mathf.Abs(delta));
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Get rect OffsetMax Y component value. (Top)
+        /// </summary>
+        protected virtual float GetOffsetMaxY(float offsetMax, float pixelHeight, LayoutData data)
+        {
+            var result = offsetMax
+                         - data.MarginTopPixels
+                         - data.OffsetTopPixels
+                         + data.OffsetBottomPixels
+                         - data.OffsetFromParentTopPixels
+                         + data.OffsetFromParentBottomPixels;
+
+            var isTopAligned = data.Alignment.HasFlag(ElementAlignment.Top);
+            var isBottomAligned = data.Alignment.HasFlag(ElementAlignment.Bottom);
+
+            // make sure top and bottom edges do not overlap if there is room to move entire view
+            var container = data.PaddedContainerPixelHeight - data.MarginTopPixels;
+            var height = pixelHeight + data.MarginBottomPixels;
+
+            // Positive delta means there is additional room to move the view in order to maintain height.
+            var delta = container - height;
+            if (delta > 0)
+            {
+                if (isBottomAligned)
+                {
+                    // Top aligned
+                    result += Mathf.Min(data.MarginBottomPixels, delta);
+                }
+                else if (!isTopAligned)
+                {
+                    // Center aligned
+                    result += Mathf.Min(data.MarginBottomPixels, delta) / 2f;
+                }
+            }
+            else if (delta < 0 && data.Height.Unit == ElementSizeUnit.Pixels)
+            {
+                // force height of pixel unit value
+                result += Mathf.Min(data.MarginBottomPixels, Mathf.Abs(delta));
             }
 
             return result;
